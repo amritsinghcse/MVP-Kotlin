e7e59f7cee12bba62f66562437abfe393f5ce03b
SuperHaker <amrits3624@gmail.com>
2018-10-14 19:15:53 +0530

    Main screen beginnning

--- app/src/main/java/com/example/android/mvpproject/MainActivity.kt
+++ app/src/main/java/com/example/android/mvpproject/MainActivity.kt
@@ -2,32 +2,15 @@ package com.example.android.mvpproject
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), MyView {
+class MainActivity : AppCompatActivity() {
 
-
-  private var userInfo: TextView? = null
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val myPresenter = MyPresenter(this)
-
-    val name = findViewById<EditText>(R.id.name) as EditText
-    val email = findViewById<EditText>(R.id.email) as EditText
-    userInfo = findViewById<TextView>(R.id.user_info) as TextView
-    val button = findViewById<Button>(R.id.submit) as Button
-    button.setOnClickListener({
-      myPresenter.updateFullName(name.text.toString())
-      myPresenter.updateEmail(email.text.toString())
-    })
-
-  }
-
-  override fun updateField(info: String) {
-    userInfo!!.text = info
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(vToolbar)
+        supportActionBar?.title = null
+    }
 }
